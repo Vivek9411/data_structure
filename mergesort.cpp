2a8c4842ef9af951164169054da32f7c76e58e6a
Vivek9411 <146237718+Vivek9411@users.noreply.github.com>
2023-10-24 10:11:04 +0530

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
+#define MAX 11
 
 using namespace std;
 
-void merge_sort (int arr[], int beg, int end)
-void merge (int arr[], int beg, int mid, int end)
+void merge_sort (int arr[], int beg, int end);
+void merge (int arr[], int beg, int mid, int end);
 
-int main ()
+int
+main ()
 {
   int n;
-  cin >> n;
   cout << "Enter the number of elements " << endl;
-  int arr[n] ;
+  cin >> n;
+  int arr[n] = { 0 };
   for (int i = 0; i < n; i++)
     {
       cin >> arr[i];
     }
-    
+// for (int i = 0; i < n; i++)
+//     {
+//       cout << arr[i] << " ";
+//     }
 //     cout<<arr[0]<<endl;
 //     cout<<2<<endl;
 //   for (int i = 0; i < n; i++)
@@ -23,33 +28,23 @@ int main ()
 //       cout << arr[i] << endl;;
 //     }
   // sort the array 
-  merge_sort (arr, 0, n-1);
+  merge_sort (arr, 0, n - 1);
 
   for (int i = 0; i < n; i++)
     {
       cout << arr[i] << " ";
     }
- return 0;
+  return 0;
 }
 
-void merge_sort (int arr[], int beg, int end)
-{
-    int mid;
-  while (beg < end)
-    {
-      mid = (beg + end) / 2;
-      merge_sort (arr, beg, mid);
-      merge_sort (arr, mid + 1, end);
-      merge (arr, beg, mid, end);
-    }
-}
+
 
 void
 merge (int arr[], int beg, int mid, int end)
 {
-  int temp[11] ;
+  int temp[MAX] = { 0 };
   int j = mid + 1;
-  int i = 0;
+  int i = beg;
   int index = beg;
 
   while ((i <= mid) && (j <= end))
@@ -66,28 +61,37 @@ merge (int arr[], int beg, int mid, int end)
 	}
       index++;
     }
-if(i>mid)
-{
+
   while (j <= end)
     {
       temp[index] = arr[j];
       j++;
       index++;
     }
-}
 
-if (j>end)
-{
   while (i <= mid)
     {
       temp[index] = arr[i];
       i++;
       index++;
-}
-  index = beg;
+    }
+
   for (int i = beg; i <= end; i++)
     {
       arr[i] = temp[i];
     }
+
 }
+
+void
+merge_sort (int arr[], int beg, int end)
+{
+  int mid;
+  if (beg < end)
+    {
+      mid = (beg + end) / 2;
+      merge_sort (arr, beg, mid);
+      merge_sort (arr, mid + 1, end);
+      merge (arr, beg, mid, end);
+    }
 }
